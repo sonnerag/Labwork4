c202c37a90c13846bb80bad8eaa391fa345f2827
binhminh123tran <minhtb.22bi13308@usth.edu.vn>
2024-08-30 13:31:24 +0700

    22bi13308

---
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,48 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String TAG = "WeatherActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i(TAG, "onCreate: Activity created");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: Activity started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: Activity resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: Activity paused");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: Activity stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: Activity destroyed");
+    }
+}
